98ff5fac81a04c782a39ba772f8c30f1171dedb7
Guillaume Ludinard <guillaume.ludinard@japannext.co.jp>
2021-11-02 09:33:04 +0900

    Logging in files
    Since we're running jobs, it's much more practical to log the debug logs
    in a file to be able to refer to it later.

--- hazelsync/cluster.py
+++ hazelsync/cluster.py
@@ -1,11 +1,13 @@
 '''Retrieve a cluster configuration'''
 
-from logging import getLogger
+from logging import getLogger, FileHandler, DEBUG, Formatter, basicConfig
+from datetime import datetime
+from pathlib import Path
 
 from hazelsync.plugin import Plugin
 from hazelsync.settings import Settings
 
-log = getLogger(__name__)
+log = getLogger('hazelsync')
 
 class Cluster:
     '''Class used to represent the cluster configuration for backup
@@ -17,15 +19,31 @@ class Cluster:
         job_type, job_options = settings.job()
         backend_type, backend_options = settings.backend()
 
+        self.name = settings.name
         self.backend = Plugin('backend').get(backend_type)(name=settings.name, **backend_options)
         self.job = Plugin('job').get(job_type)(name=settings.name, **job_options, backend=self.backend)
 
+    def config_logging(self, action: str):
+        '''Configure the logging'''
+        path = Path(f'/var/log/hazelsync/{self.name}')
+        path.mkdir(exist_ok=True, parents=True)
+        now = datetime.now().strftime('%Y-%m-%dT%H%M%S')
+        formatter = Formatter('%(asctime)s %(levelname)s: %(message)s')
+        handler = FileHandler(path / f"{now}-{action}.log")
+        filename = path / f"{now}-{action}.log"
+        handler.setLevel(DEBUG)
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+        basicConfig(filename=filename, format='%(asctime)s %(levelname)s: %(message)s', level=DEBUG)
+
     def backup(self):
         '''Run the backup of a cluster'''
+        self.config_logging('backup')
         slots = self.job.backup()
         for slot in slots:
             self.backend.snapshot(slot)
 
     def restore(self, snapshot):
         '''Restore a snapshot on a cluster'''
+        self.config_logging('restore')
         self.job.restore(snapshot)
